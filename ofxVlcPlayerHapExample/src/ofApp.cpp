a53229eadf2b3308b5f4fcc8d558eca645670370
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2024-06-16 09:49:54 +0200

    Update ofApp.cpp

--- ofxVlcPlayerHapExample/src/ofApp.cpp
+++ ofxVlcPlayerHapExample/src/ofApp.cpp
@@ -4,7 +4,7 @@
 void ofApp::setup() {
     ofSetWindowTitle("ofxVlcPlayerHapExample");
     ofSetFrameRate(30);
-    char const* vlc_argv[] = { "--vout=opengl" };
+    char const* vlc_argv[] = { "--vout=opengl", "--start-paused" };
     int vlc_argc = sizeof(vlc_argv) / sizeof(*vlc_argv);
     player.load("data/Loop1_All Color.mov", vlc_argc, vlc_argv);
     player.setLoop(true);
@@ -20,7 +20,7 @@ void ofApp::update() {
 void ofApp::draw() {
     player.draw(0, 0, 1280, 720);
     ofDrawBitmapString("FPS: " + ofToString(ofGetFrameRate()), 20, 20);
-    ofDrawBitmapString("Second: " + ofToString(player.getPosition() * player.getLength() / 1000), 20, 40);
+    ofDrawBitmapString("Second: " + ofToString(player.getTime() / 1000), 20, 40);
 }
 
 //--------------------------------------------------------------
