b7ddf66d3e4cb9e1b2f259b1ee49088273be12dc
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2024-11-21 00:35:32 +0100

    Update ofApp.cpp

--- ofxVlcPlayerExample/src/ofApp.cpp
+++ ofxVlcPlayerExample/src/ofApp.cpp
@@ -19,7 +19,7 @@ void ofApp::update() {
 //--------------------------------------------------------------
 void ofApp::draw() {
     ofSetColor(255);
-    player.getTexture().draw(0, 0);
+    player.draw(0, 0);
     ofSetColor(0, 255, 0);
     ofDrawBitmapString("FPS: " + ofToString(ofGetFrameRate()), 20, 20);
     ofDrawBitmapString("Second: " + ofToString(player.getTime() / 1000), 20, 40);
