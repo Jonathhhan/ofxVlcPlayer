00ae99244e592fc5b14f362da961fe8e036dd88a
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2024-11-21 02:48:13 +0100

    Add files via upload

--- src/ofxVlcPlayer.cpp
+++ src/ofxVlcPlayer.cpp
@@ -4,6 +4,8 @@
 ofxVlcPlayer::ofxVlcPlayer()
     : libvlc(NULL), eventManager(NULL), media(NULL), mediaPlayer(NULL), videoHeight(0), videoWidth(0), tex(), fbo() {
     ofGLFWWindowSettings settings;
+    settings.shareContextWith = ofGetCurrentWindow();
+    vlcWindow = std::make_shared<ofAppGLFWWindow>();
     vlcWindow->setup(settings);
     vlcWindow->setVerticalSync(true);
     texture.allocate(1, 1, GL_RGBA);
