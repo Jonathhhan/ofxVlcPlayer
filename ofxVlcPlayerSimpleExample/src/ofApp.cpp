2d50a8d6143260375739a67800669a86f2e403ac
Jonathan Frank <41275844+Jonathhhan@users.noreply.github.com>
2024-11-24 04:28:20 +0100

    Add files via upload

---
+++ ofxVlcPlayerSimpleExample/src/ofApp.cpp
@@ -0,0 +1,62 @@
+#include "ofApp.h"
+
+//--------------------------------------------------------------
+void ofApp::setup() {
+    ofDisableArbTex();
+    ofSetWindowTitle("ofxVlcPlayerSimpleExample");
+    ofSetFrameRate(60);
+    char const* vlc_argv[] = { "--input-repeat=100", "--rate=1" };
+    int vlc_argc = sizeof(vlc_argv) / sizeof(*vlc_argv);
+    player.load("data/Big_Buck_Bunny_1080_10s_30MB.mp4", vlc_argc, vlc_argv);
+    player.setLoop(true);
+    player.play(); 
+}
+
+//--------------------------------------------------------------
+void ofApp::update() {
+    player.update();
+}
+
+//--------------------------------------------------------------
+void ofApp::draw() {
+    player.draw(0, 0, 1280, 720);
+    ofDrawBitmapStringHighlight("FPS: " + ofToString(ofGetFrameRate()), 20, 20);
+    ofDrawBitmapStringHighlight("Second: " + ofToString(player.getTime() / 1000), 20, 40);
+}
+
+//--------------------------------------------------------------
+void ofApp::keyPressed(int key) {
+    player.pause();
+}
+
+//--------------------------------------------------------------
+void ofApp::keyReleased(int key) {
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseMoved(int x, int y) {
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseDragged(int x, int y, int button) {
+}
+
+//--------------------------------------------------------------
+void ofApp::mousePressed(int x, int y, int button) {
+}
+
+//--------------------------------------------------------------
+void ofApp::mouseReleased(int x, int y, int button) {
+}
+
+//--------------------------------------------------------------
+void ofApp::windowResized(int w, int h) {
+}
+
+//--------------------------------------------------------------
+void ofApp::gotMessage(ofMessage msg) {
+}
+
+//--------------------------------------------------------------
+void ofApp::dragEvent(ofDragInfo dragInfo) {
+}
